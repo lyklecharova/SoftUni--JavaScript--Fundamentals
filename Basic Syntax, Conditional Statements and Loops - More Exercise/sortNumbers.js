9ed65356e0f6afc038c4139de18abf0413d923c3
Lilyana Klecharova <111914791+lyklecharova@users.noreply.github.com>
2022-09-19 13:40:48 +0300

    Update sortNumbers.js

--- Basic Syntax, Conditional Statements and Loops - More Exercise/sortNumbers.js
+++ Basic Syntax, Conditional Statements and Loops - More Exercise/sortNumbers.js
@@ -1,8 +1,19 @@
+
 function sortNumber(number1, number2, number3) {
-  let num = [number1, number2, number3];
-  let sortedNumber = num.sort((a, b) => b - a);
-  console.log(num.join('\n'));
+  let num = [number1, number2, number3].sort((a, b) => b - a);
+
+  console.log(num.join("\n"));
+
 
 }
+
+
+// function sortNumber(number1, number2, number3) {
+//   let num = [number1, number2, number3];
+//   let sortedNumber = num.sort((a, b) => b - a);
+//   console.log(num.join('\n'));
+
+// }
+
 
 
