60726803bdd6245629b04e79b389ca3ff5d81418
Lilyana Klecharova <111914791+lyklecharova@users.noreply.github.com>
2022-09-19 13:43:49 +0300

    Add files via upload

---
+++ Basic Syntax, Conditional Statements and Loops - More Exercise/distanceBetweenPoints.js
@@ -0,0 +1,13 @@
+function distanceBetweenPoints(x1, y1, x2, y2) {
+    // we will use pythagorean theorem
+    // a^2 + b^2 = c^2
+    // a & b are both sides of 90 deg triangle
+    // c = Math.Sqrt(x^2 + b^2)
+    // is the hypotenuse
+    let a = Math.abs(x1 - x2);
+    let b = Math.abs(y1 - y2);
+
+    // distance is "c"
+    let distance = Math.sqrt((a * a) + (b * b));
+    console.log(distance);
+}
---
+++ Basic Syntax, Conditional Statements and Loops - More Exercise/englishNameOfTheLastDigit.js
@@ -0,0 +1,35 @@
+function englishNameOfTheLastDigit(input) {
+    let lastElement = String(input);
+    let last = lastElement.length;
+
+    if (lastElement[last - 1] == 0) {
+        console.log("zero");
+    }
+    if (lastElement[last - 1] == 1) {
+        console.log("one");
+    }
+    if (lastElement[last - 1] == 2) {
+        console.log("two");
+    }
+    if (lastElement[last - 1] == 3) {
+        console.log("three");
+    }
+    if (lastElement[last - 1] == 4) {
+        console.log("four");
+    }
+    if (lastElement[last - 1] == 5) {
+        console.log("five");
+    }
+    if (lastElement[last - 1] == 6) {
+        console.log("six");
+    }
+    if (lastElement[last - 1] == 7) {
+        console.log("seven");
+    }
+    if (lastElement[last - 1] == 8) {
+        console.log("eight");
+    }
+    if (lastElement[last - 1] == 9) {
+        console.log("nine");
+    }
+}
