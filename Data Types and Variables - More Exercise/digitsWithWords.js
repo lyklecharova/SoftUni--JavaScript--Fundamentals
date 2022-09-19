5881a8b00ca6974c039037da0ea414a8545b35fc
Lilyana Klecharova <111914791+lyklecharova@users.noreply.github.com>
2022-09-19 13:45:07 +0300

    Add files via upload

---
+++ Data Types and Variables - More Exercise/digitsWithWords.js
@@ -0,0 +1,14 @@
+function digitsWithWords(input) {
+    switch (input) {
+        case 'zero': console.log(0); break;
+        case 'one': console.log(1); break;
+        case 'two': console.log(2); break;
+        case "three": console.log(3); break;
+        case 'four': console.log(4); break;
+        case 'five': console.log(5); break;
+        case 'six': console.log(6); break;
+        case 'seven': console.log(7); break;
+        case 'eight': console.log(8); break;
+        case 'nine': console.log(9); break;
+    }
+}
