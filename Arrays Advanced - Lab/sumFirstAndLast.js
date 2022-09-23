7ee4d757e161a26c0e79815c485fb5350b92a774
Lilyana Klecharova <111914791+lyklecharova@users.noreply.github.com>
2022-09-23 09:18:39 +0300

    Delete Arrays Advanced - Lab directory

--- Arrays Advanced - Lab/sumFirstAndLast.js
+++
@@ -1,8 +0,0 @@
-function sumFirstAndLast(array) {
-    let firstEl = array.shift();
-    let lastEl = array.pop();
-
-    console.log(Number(firstEl) + Number(lastEl));
-
-}
-sumFirstAndLast(['20', '30', '40'])
