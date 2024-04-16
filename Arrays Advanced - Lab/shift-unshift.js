e859a58824c74c789f7fa5454e3d32fbf71d4ad1
lyklecharova <lklecharova9@gmail.com>
2024-04-16 10:00:19 +0300

    Create shift-unshift.js

---
+++ Arrays Advanced - Lab/shift-unshift.js
@@ -0,0 +1,10 @@
+let cars = ['BWM', 'Mercedess', 'Toyota'];
+
+//! unshift() -  add the element at the beginning of the array
+cars.unshift('Lada');
+console.log(cars);
+
+//! shift() - remove the  first element in the array
+let firstCar = cars.shift();
+console.log(cars);
+console.log(firstCar);
