d77933979ac9798a78b879541f7f37a915fd2c5d
lyklecharova <lklecharova9@gmail.com>
2024-04-18 09:37:18 +0300

    Create object-destructuring.js

---
+++ Associative Arrays/object-destructuring.js
@@ -0,0 +1,10 @@
+let phoneBook = {
+    ivaylo: '11651654',
+    pesho: '11651623',
+    gosho: '11651634',
+    stamat: '11651645',
+    maryika: '11651625',
+};
+
+//! object destrcuturing
+let { maryika, gosho } = phoneBook;
